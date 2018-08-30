baf82eb95d4ebd128adaff18ae9d2ac378600b0b
Bruno Martins <bgcngm@gmail.com>
2018-08-31 00:20:05 +0100

    LockClock: Declare private API dependency
     * Addresses the following compilation issue:
    
       error: LockClock (java:sdk) should not link to org.lineageos.platform.internal (java:platform)
    
    Change-Id: Ic3635deb1f9842aa82e90dd2c8b4a04ceb8f6654

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_SDK_VERSION := current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PACKAGE_NAME := LockClock
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
